0b5fb20cc676f0efd3e660ccbb67ec4e66442615
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-16 21:13:36 +0530

    Update getAllShops.jsx

--- frontend/src/hooks/getAllShops.jsx
+++ frontend/src/hooks/getAllShops.jsx
@@ -1,50 +1,25 @@
-// import axios from 'axios'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { serverUrl } from '../App'
-// import { setAllShops } from '../redux/userSlice'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { serverUrl } from '../App'
+import { setAllShops } from '../redux/userSlice'
 
-// function getAllShops() {
-//    const dispatch = useDispatch()
-//    useEffect(() => {
-//       const fetchShops = async () => {
-//          const result = await axios.get(`${serverUrl}/api/shop/getall`, { withCredentials: true })
-//          if (result) {
-//             dispatch(setAllShops(result.data))
-//          }
+function getAllShops() {
+   const dispatch = useDispatch()
+   useEffect(() => {
+      const fetchShops = async () => {
+         const result = await axios.get(`${serverUrl}/api/shop/getall`, { withCredentials: true })
+         if (result) {
+            dispatch(setAllShops(result.data))
+         }
 
-//       }
-//       fetchShops()
-//    }, [])
-// }
-
-// export default getAllShops;
-
-
-
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { serverUrl } from "../App"
-import { setAllShops } from "../redux/userSlice"
-
-function useGetAllShops() {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    const fetchShops = async () => {
-      try {
-        const result = await axios.get(`${serverUrl}/api/shop/getall`)
-        if (result.data) {
-          dispatch(setAllShops(result.data))
-        }
-      } catch (error) {
-        console.error("Error fetching shops:", error.response?.data || error.message)
       }
-    }
-
-    fetchShops()
-  }, [dispatch])
+      fetchShops()
+   }, [])
 }
 
-export default useGetAllShops
+export default getAllShops;
+
+
+
+
