910375969574eb617d49afc9dae9a948569b681d
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-24 10:03:37 +0530

    Update getAllShops.jsx

--- frontend/src/hooks/getAllShops.jsx
+++ frontend/src/hooks/getAllShops.jsx
@@ -1,64 +1,22 @@
-// import axios from 'axios'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { serverUrl } from '../App'
-// import { setAllShops } from '../redux/userSlice'
-
-// function getAllShops() {
-//    const dispatch = useDispatch()
-//    useEffect(() => {
-//       const fetchShops = async () => {
-//          const result = await axios.get(`${serverUrl}/api/shop/getall`, { withCredentials: true })
-//          if (result) {
-//             dispatch(setAllShops(result.data))
-//          }
-
-//       }
-//       fetchShops()
-//    }, [])
-// }
-
-// export default getAllShops;
-
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { serverUrl } from "../App";
-import { setAllShops } from "../redux/userSlice";
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { serverUrl } from '../App'
+import { setAllShops } from '../redux/userSlice'
 
 function getAllShops() {
-  const dispatch = useDispatch();
+   const dispatch = useDispatch()
+   useEffect(() => {
+      const fetchShops = async () => {
+         const result = await axios.get(`${serverUrl}/api/shop/getall`, { withCredentials: true })
+         if (result) {
+            dispatch(setAllShops(result.data))
+         }
 
-  useEffect(() => {
-    const fetchShops = async () => {
-      try {
-        const token = localStorage.getItem("token"); // ✅ get token from storage
-
-        const result = await axios.get(
-          `${serverUrl}/api/shop/getAll`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`, // ✅ send token
-            },
-            withCredentials: true, // in case backend uses cookies also
-          }
-        );
-
-        if (result) {
-          dispatch(setAllShops(result.data));
-        }
-      } catch (err) {
-        console.error("Error fetching shops:", err.response?.data || err.message);
       }
-    };
-
-    fetchShops();
-  }, []);
-
-  return null;
+      fetchShops()
+   }, [])
 }
 
 export default getAllShops;
-
-
 
