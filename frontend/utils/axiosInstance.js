072dc581e8401ddf5204b8af57e2fcf5a04296fa
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-25 16:23:46 +0530

    Update axiosInstance.js

--- frontend/utils/axiosInstance.js
+++ frontend/utils/axiosInstance.js
@@ -1,1 +1,22 @@
+import axios from "axios";
+import { serverUrl } from "../App";
+
+const axiosInstance = axios.create({
+  baseURL: serverUrl,
+  withCredentials: true,
+});
+
+// Attach token automatically to every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
 
