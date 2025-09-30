a8c691ca008bbf040256cf394f292062d95e6f0d
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-30 14:11:38 +0530

    Update getCurrentUser.jsx

--- frontend/src/hooks/getCurrentUser.jsx
+++ frontend/src/hooks/getCurrentUser.jsx
@@ -1,26 +1,4 @@
-// import axios from 'axios'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { serverUrl } from '../App'
-// import { setUserData } from '../redux/userSlice'
 
-// function useCurrentUser() {
-//     const dispatch = useDispatch()
-//     useEffect(() => {
-//         const fetchUsers = async () => {
-//             const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
-//             console.log(result.data)
-//             dispatch(setUserData(result.data))
-//         }
-//         fetchUsers()
-//     }, [dispatch])
-// }
-
-// export default useCurrentUser
-// export { useCurrentUser as getCurrentUser }
-
-
-// useCurrentUser.js
 import axios from 'axios';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
