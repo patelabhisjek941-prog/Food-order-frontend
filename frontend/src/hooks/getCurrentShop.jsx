91c7b8482b1b9fc2c18736da99c535c64822a3ab
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-30 14:11:17 +0530

    Update getCurrentShop.jsx

--- frontend/src/hooks/getCurrentShop.jsx
+++ frontend/src/hooks/getCurrentShop.jsx
@@ -33,32 +33,3 @@ function useCurrentShop() {
 export default useCurrentShop;
 export { useCurrentShop as getCurrentShop };
 
-
-// import axios from 'axios'
-// import { useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { serverUrl } from '../App'
-// import { setShop } from '../redux/userSlice'
-
-// function useCurrentShop() {
-//     const dispatch = useDispatch()
-//     const { userData } = useSelector(state => state.user)
-
-//     useEffect(() => {
-//         if (userData?.role === "owner") {
-//             const fetchShop = async () => {
-//                 try {
-//                     const result = await axios.get(`${serverUrl}/api/shop/getcurrent`, { withCredentials: true })
-//                     dispatch(setShop(result.data))
-//                 } catch (err) {
-//                     console.error("Failed to fetch shop:", err)
-//                 }
-//             }
-//             fetchShop()
-//         }
-//     }, [userData, dispatch])
-// }
-
-// export default useCurrentShop
-// export { useCurrentShop as getCurrentShop }
-
