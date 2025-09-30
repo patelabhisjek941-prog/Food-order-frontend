890a808afbdbe5b21e136e5ba82834ae1570b907
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-30 14:09:55 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,3 @@
-// import { useSelector } from 'react-redux';
-// // import UserDashboard from '../components/userDashboard';
-// import UserDashboard from "../components/UserDashboard";
-
-
-// import DeliveryBoy from '../components/DeliveryBoy';
-// import Footer from '../components/Footer';
-// import OwnerDashboard from '../components/OwnerDashboard';
-// function Home() {
-//   const { userData } = useSelector(state => state.user)
-//   return (
-//     <div className='w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center bg-[#fff9f6]'>
-//       {userData?.role == "user" && <UserDashboard />}
-//       {userData?.role == "owner" && <OwnerDashboard />}
-//       {userData?.role == "deliveryBoy" && <DeliveryBoy />}
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default Home
-
 
 import { useSelector } from 'react-redux';
 import UserDashboard from "../components/UserDashboard";
