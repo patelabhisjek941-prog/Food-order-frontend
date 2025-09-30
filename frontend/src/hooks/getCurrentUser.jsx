9877d5e9945e5819389c1c7088411524b5fae023
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-30 12:51:58 +0530

    Update getCurrentUser.jsx

--- frontend/src/hooks/getCurrentUser.jsx
+++ frontend/src/hooks/getCurrentUser.jsx
@@ -1,21 +1,50 @@
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { serverUrl } from '../App'
-import { setUserData } from '../redux/userSlice'
+// import axios from 'axios'
+// import { useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { serverUrl } from '../App'
+// import { setUserData } from '../redux/userSlice'
+
+// function useCurrentUser() {
+//     const dispatch = useDispatch()
+//     useEffect(() => {
+//         const fetchUsers = async () => {
+//             const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
+//             console.log(result.data)
+//             dispatch(setUserData(result.data))
+//         }
+//         fetchUsers()
+//     }, [dispatch])
+// }
+
+// export default useCurrentUser
+// export { useCurrentUser as getCurrentUser }
+
+
+// useCurrentUser.js
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { serverUrl } from '../App';
+import { setUserData } from '../redux/userSlice';
 
 function useCurrentUser() {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
-            console.log(result.data)
-            dispatch(setUserData(result.data))
-        }
-        fetchUsers()
-    }, [dispatch])
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const { data } = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
+        dispatch(setUserData(data));
+      } catch (error) {
+        console.error("Failed to fetch current user:", error.response?.data?.message || error.message);
+      }
+    };
+
+    fetchUser();
+  }, [dispatch]);
 }
 
-export default useCurrentUser
-export { useCurrentUser as getCurrentUser }
+export default useCurrentUser;
+export { useCurrentUser as getCurrentUser };
+
 
